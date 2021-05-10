ab5747672bb51191a9930915ad3f78c3265d1da6
Sirius-Development <griffinferguson.us@gmail.com>
2021-05-10 14:09:04 -0500

    Side panel functions
    Keep working on those

--- resources/site_scripts.js
+++ resources/site_scripts.js
@@ -1,14 +1,43 @@
 //Side panel
-function sidePanel(element) {
+function sidePanel(element, method) {
     var panel = null;
+    var otherPanel = null;
+    var trigger = null;
+
     if (element == controls) {
+      console.log("controls")
       panel = document.getElementById("controls");
+      otherPanel = document.getElementById("about");
+      trigger = document.getElementById("control-button");
+      if (method == 'open') {
+        console.log("open")
+        panel.classList.add("open-panel");
+        setTimeout(() => {panel.classList.add("display-content")}, 200);
+        trigger.setAttribute("onclick", "sidePanel(controls, 'close')")
+        sidePanel(otherPanel, 'close');
+      }
+      if (method == 'close') {
+        console.log("close")
+        panel.classList.remove("open-panel");
+        panel.classList.remove("display-content");
+        trigger.setAttribute("onclick", "sidePanel(controls, 'open')")
+      }
     }
 
     if (element == about) {
-        panel = document.getElementById("about");
+      panel = document.getElementById("about");
+      otherPanel = document.getElementById("controls");
+      trigger = document.getElementById("about-button");
+      if (method == 'open') {
+        panel.classList.add("open-panel");
+        setTimeout(() => {panel.classList.add("display-content")}, 200);
+        trigger.setAttribute("onclick", "sidePanel(about, 'close')")
+        sidePanel(otherPanel, 'close');
       }
-    console.log(panel);
-    panel.classList.toggle("open-panel");
-    panel.classList.toggle("display-content");
-  }
+      if (method == 'close') {
+        panel.classList.remove("open-panel");
+        panel.classList.remove("display-content");
+        trigger.setAttribute("onclick", "sidePanel(about, 'open')")
+      }
+    }
+}
