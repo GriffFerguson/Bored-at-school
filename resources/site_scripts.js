ee4465a1649e61dd72ab54e02897001668a2baee
Sirius-Development <griffinferguson.us@gmail.com>
2021-05-10 21:34:23 -0500

    Work on embed links/styling

--- resources/site_scripts.js
+++ resources/site_scripts.js
@@ -1,43 +1,56 @@
-//Side panel
+//SIDE PANEL
 function sidePanel(element, method) {
-    var panel = null;
-    var otherPanel = null;
-    var trigger = null;
+	var panel = null;
+	var otherPanel = null;
+	var trigger = null;
 
-    if (element == controls) {
-      console.log("controls")
-      panel = document.getElementById("controls");
-      otherPanel = document.getElementById("about");
-      trigger = document.getElementById("control-button");
-      if (method == 'open') {
-        console.log("open")
-        panel.classList.add("open-panel");
-        setTimeout(() => {panel.classList.add("display-content")}, 200);
-        trigger.setAttribute("onclick", "sidePanel(controls, 'close')")
-        sidePanel(otherPanel, 'close');
-      }
-      if (method == 'close') {
-        console.log("close")
-        panel.classList.remove("open-panel");
-        panel.classList.remove("display-content");
-        trigger.setAttribute("onclick", "sidePanel(controls, 'open')")
-      }
-    }
+	if (element == controls) {
+		console.log("controls")
+		panel = document.getElementById("controls");
+		otherPanel = document.getElementById("about");
+		trigger = document.getElementById("control-button");
+		if (method == 'open') {
+			console.log("open")
+			panel.classList.add("open-panel");
+			setTimeout(() => {panel.classList.add("display-content")}, 200);
+			trigger.setAttribute("onclick", "sidePanel(controls, 'close')")
+			sidePanel(otherPanel, 'close');
+		}
+		if (method == 'close') {
+			console.log("close")
+			panel.classList.remove("open-panel");
+			panel.classList.remove("display-content");
+			trigger.setAttribute("onclick", "sidePanel(controls, 'open')")
+		}
+	}
 
-    if (element == about) {
-      panel = document.getElementById("about");
-      otherPanel = document.getElementById("controls");
-      trigger = document.getElementById("about-button");
-      if (method == 'open') {
-        panel.classList.add("open-panel");
-        setTimeout(() => {panel.classList.add("display-content")}, 200);
-        trigger.setAttribute("onclick", "sidePanel(about, 'close')")
-        sidePanel(otherPanel, 'close');
-      }
-      if (method == 'close') {
-        panel.classList.remove("open-panel");
-        panel.classList.remove("display-content");
-        trigger.setAttribute("onclick", "sidePanel(about, 'open')")
-      }
-    }
+	if (element == about) {
+		panel = document.getElementById("about");
+		otherPanel = document.getElementById("controls");
+		trigger = document.getElementById("about-button");
+		if (method == 'open') {
+			panel.classList.add("open-panel");
+			setTimeout(() => {panel.classList.add("display-content")}, 200);
+			trigger.setAttribute("onclick", "sidePanel(about, 'close')")
+			sidePanel(otherPanel, 'close');
+		}
+		if (method == 'close') {
+			panel.classList.remove("open-panel");
+			panel.classList.remove("display-content");
+			trigger.setAttribute("onclick", "sidePanel(about, 'open')")
+		}
+	}
+}
+
+//EMBED
+function embed() {
+	var path = window.location.pathname;
+	var dir = path.substring(0, path.lastIndexOf('/'));
+	var location = "https://boredatschool.netlify.app" + dir + "/embed.html";
+	document.getElementById("embed-link").innerHTML +=
+	'<span style="text-decoration:underline;">' + location + '</span>'
+}
+
+function toggleEmbed() {
+	document.getElementById("embed-link").classList.toggle("open-embed");
 }
